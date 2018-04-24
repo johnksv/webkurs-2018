75bd464c63edd0b3b9d6ff5ee378ee1329be08b6
Mathilde Ørstavik <mathilde.orstavik@norkart.no>
2018-04-24 14:30:24 +0200

    upda

--- code-example/public/js/main.js
+++ code-example/public/js/main.js
@@ -9,16 +9,12 @@ function setMap() {
             container: 'map', // container id
             style: 'mapbox://styles/mapbox/streets-v9', // stylesheet location
             center: [10.74812, 59.92145], // starting position [lng, lat]
-            zoom: 14 // starting zoom
+            zoom: 14 // starting zoom. Zoomlevel is from 0 -> 22, where 22 is zoomed in an 0 is zoomed out
       });
-      //var map = L.map('map').setView([58.969976, 5.733107], 14);
-      //Set view takes two parameters;
-            //1. The coordinates for the center of the map
-            //2. The zoom level. Zoomlevel is from 0 -> 22, where 22 is zoomed in an 0 is zoomed out
+
+      map.addControl(new mapboxgl.NavigationControl());
 
       //Adding geoJSON layer to the map:
-      //L.geoJSON(restaurants).addTo(map);
-
       map.on('load', function(){
             map.addLayer(createMapboxLayer(osloUtesteder));
       });
--- task-boilerplate/public/js/main.js
+++ task-boilerplate/public/js/main.js
@@ -4,18 +4,35 @@ function setMap() {
 
       console.log('Loading map');
 
-      var map = L.map('map').setView([58.969976, 5.733107], 14);
-      //Set view takes two parameters;
-            //1. The coordinates for the center of the map
-            //2. The zoom level. Zoomlevel is from 0 -> 22, where 22 is zoomed in an 0 is zoomed out
-
-
-      //Adding the base map. Base map decides how the background map looks like
-      var basemapUrl='http://{s}.tile.osm.org/{z}/{x}/{y}.png';
-      L.tileLayer(basemapUrl).addTo(map);
+      mapboxgl.accessToken = 'pk.eyJ1IjoibWF0aGlsZG8iLCJhIjoiY2lrdHZvMHdsMDAxMHdvbTR0MWZkY3FtaCJ9.u4bFYLBtEGNv4Qaa8Uaqzw';
+      var map = new mapboxgl.Map({
+            container: 'map', // container id
+            style: 'mapbox://styles/mapbox/streets-v9', // stylesheet location
+            center: [10.74812, 59.92145], // starting position [lng, lat]
+            zoom: 14 // starting zoom. Zoomlevel is from 0 -> 22, where 22 is zoomed in an 0 is zoomed out
+      });
+      //Adding navigation controls
+      map.addControl(new mapboxgl.NavigationControl());
 
       //Adding geoJSON layer to the map:
-      L.geoJSON(restaurants).addTo(map);
+      map.on('load', function(){
+            map.addLayer(createMapboxLayer(osloUtesteder));
+      });
 }
 
+function createMapboxLayer(geojson) {
+      var layer =  {
+            'id': 'utesteder',
+            'type': 'circle',
+            'source': {
+                  'type': 'geojson',
+                  'data': geojson
+            },
+            'layout': { //styling of the features
+            }
+      }
+      return layer;
+};
+
 window.onload = setMap;
+
